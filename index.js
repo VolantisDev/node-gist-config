bab4f58588a4ad6c2527b0cd7b36777f448dc642
Ben McClure <ben.mcclure@gmail.com>
2017-06-30 04:03:59 -0500

    In progress

---
+++ index.js
@@ -0,0 +1,19 @@
+const configure = require('./lib/configure')
+const sync = require('./lib/sync')
+const download = require('./lib/download')
+const upload = require('./lib/upload')
+const create = require('./lib/create')
+
+module.exports = (cfg) => {
+    const config = require('./lib/config')(cfg)
+    
+    return {
+        config: config,
+        configure: () => configure(config),
+        reconfigure: () => configure(config, true),
+        sync: (data) => sync(config, data),
+        download: (data) => download(config, data),
+        upload: (data) => upload(config, data),
+        create: (data) => create(config, data)
+    }
+}
