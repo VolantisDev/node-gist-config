7709a6950baa416d174c194447088b8caf5143fc
Ben McClure <ben.mcclure@gmail.com>
2017-06-30 17:37:59 -0500

    A bunch of changes prior to revamping config handling

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const sync = require('./lib/sync')
 const download = require('./lib/download')
 const upload = require('./lib/upload')
 const create = require('./lib/create')
+const reset = require('./lib/reset')
 
 module.exports = (cfg) => {
     const config = require('./lib/config')(cfg)
@@ -14,6 +15,7 @@ module.exports = (cfg) => {
         sync: (data) => sync(config, data),
         download: (data) => download(config, data),
         upload: (data) => upload(config, data),
-        create: (data) => create(config, data)
+        create: (data) => create(config, data),
+        reset: () => reset(config)
     }
 }
