ca724840700f3942aea05dedc380a5151196be78
Ben McClure <ben.mcclure@gmail.com>
2017-07-04 11:02:35 -0500

    Revamped to use gist-packer

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
-const configure = require('./lib/configure')
-const sync = require('./lib/sync')
-const download = require('./lib/download')
-const upload = require('./lib/upload')
-const create = require('./lib/create')
-const reset = require('./lib/reset')
-const config = require('./lib/config')
-
 module.exports = (cfg) => {
-    if (cfg) {
-        config.set(cfg)
-    }
+    const config = require('./lib/config').set(cfg)
+    const cli = require('./lib/cli')(config)
+    const sync = require('./lib/operations/sync')
+    const download = require('./lib/operations/download')
+    const upload = require('./lib/operations/upload')
+    const create = require('./lib/operations/create')
+    const reset = require('./lib/operations/reset')
     
     return {
-        config: config,
-        configure: () => configure(config),
-        reconfigure: () => configure(config, true),
+        config,
+        cli,
         sync: (data) => sync(config, data),
         download: (data) => download(config, data),
         upload: (data) => upload(config, data),
